4b4784568f85a5b79b4bc5b155a687ce2704ca81
Juan Carlos Vazquez <juancgvazquez@gmail.com>
2025-05-07 19:07:51 +0200

    replacing setup with pyproject

--- padel7_booking_bot/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-
-setup(
-    name="my-booking-bot",
-    version="0.1",
-    description="A bot for booking padel courts",
-    packages=find_packages(),
-)
